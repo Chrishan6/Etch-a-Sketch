1bd4784b92e5fc0f0b285047d9de389b9c8ef2cc
Chrishan6 <chris.haniak@gmail.com>
2024-04-10 21:12:12 -0700

    Append gridContainer to etchContainer Switch createGrid size values to fluid instead of 8 via grid-column & grid-row var's Add gridContainer.innerHTML to debug pre-existing squares Change call of createGrid from 8 to 20 to test new grid size variables Add handleHover & handlePaint functions to allow for mouse related event listeners - hover & click Loop both handleHover & handlePaint into square to make sure each square has mouse functionality Append etchContainer to body Change grid-template-rows & columns to have --grid variables in CSS Switch border of #gridContainer to 5px and blue Switch script tag location in HTML to body for debugging purposes

--- e-a-s.js
+++ e-a-s.js
@@ -2,9 +2,17 @@ const etchContainer = document.querySelector("#etch-container")
 
 const gridContainer = document.querySelector("#grid-container")
 
+etchContainer.appendChild(gridContainer)
+
 
 //creating the grid size
 function createGrid(size) {
+    gridContainer.style.setProperty("--grid-columns", size)
+    gridContainer.style.setProperty("--grid-rows", size)
+
+    //remove any pre-existing squares (if any)
+    gridContainer.innerHTML = ""
+
     //loop to generate the squares in the grid
     for(let i = 0; i < size * size; i++) {
         //setting up square div for css
@@ -14,13 +22,61 @@ function createGrid(size) {
     }
 }
 
-document.body.appendChild(etchContainer)
+createGrid(20)
 
-const squares = document.querySelector(".square")
+const squares = document.querySelectorAll(".square")
 
 //to determine if mouse is dragging or not
 let isDrawing = false
 
-createGrid(8)
+function handleHover(square) {
+    //change colour when hovering over with mouse
+    square.addEventListener("mouseenter", () => {
+        if (isDrawing) {
+            square.style.backgroundColor = "white"
+        } else {
+            square.style.backgroundColor = "blue"
+        }
+    })
+
+    square.addEventListener("mouseleave", () =>{
+        if (!square.classList.contains("hovered")) {
+            square.style.backgroundColor = "white"
+        }
+    })
+}
+
+
+//clicking functionality to make sure the squares remain painted when dragging the mouse
+
+//clicking square
+function handlePaint(square){
+square.addEventListener("mousedown", () =>{
+    isDrawing = true
+    square.classList.add("hovered")   
+})
+//while not pressing down
+square.addEventListener("mouseup", () =>{
+    isDrawing = false
+})
+
+//dragging while pressing down
+square.addEventListener("mousemove", () => {
+    if(isDrawing = true){
+        square.style.backgroundColor = "blue"
+        square.classList.add("hovered")
+    }
+})
+}
+
+//attaching handleHover and handlePaint to each square
+squares.forEach(square => {
+   handleHover(square)
+   handlePaint(square)
+});
+
+
+
+document.body.appendChild(etchContainer)
 
 console.log("gridContainer", gridContainer)
